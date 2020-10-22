c7a09d4f62434ed7f5b807d88b0e1915770999fa
Ana Eliza Bastos Martins de Freitas <ana.eliza@squadra.com.br>
2020-10-22 12:04:10 -0300

    Implementação do método Delete

--- WebAPI/Controllers/ContaPFController.cs
+++ WebAPI/Controllers/ContaPFController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -23,7 +24,101 @@ namespace Financial.WebAPI.Controllers
             "João", "Maria", "Creuza", "José"
         };
 
+        private readonly ILogger<ContaPFController> _logger;
 
+        public ContaPFController(ILogger<ContaPFController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        /// <summary>
+        /// Lista de contas pessoa física
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<ContaPF> GetAll()
+        {
+            //var rng = new Random();
+            //return Enumerable.Range(1, 1).Select(index => new ContaPF //Arrow Function
+            //{
+            //    Agencia = rng.Next(1111, 9999),
+            //    Conta = rng.Next(111111, 999999),
+            //    TipoConta = TipoConta[rng.Next(TipoConta.Length)],
+            //    NomeCompleto = Nome[rng.Next(Nome.Length)],
+            //})
+            //.ToList();
+
+            return GerarLista();
+        }
+
+
+        /// <summary>
+        /// Filtro de lista de conta pessoa física, pelo id da conta
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ContaPF GetById(int id)
+        {
+            return GerarLista()
+                .FirstOrDefault(conta => conta.Id == id);
+        }
+
+        // POST api/<ContaPFController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ContaPFController>/5
+        [HttpPut("{id}")]
+        public ContaPF Put(int id, [FromBody] ContaPF contaPF)
+        {
+            var conta = GetById(id);
+
+            conta.Agencia = contaPF.Agencia;
+            conta.Conta = contaPF.Conta;
+            conta.TipoConta = contaPF.TipoConta;
+            conta.NomeCompleto = contaPF.NomeCompleto;
+
+            return conta;
+        }
+
+        /// <summary>
+        /// Método que remove um item da lista, filtrado pelo id.
+        /// </summary>
+        /// <param name="idParam"></param>
+        /// <returns></returns>
+        /// <response code="404">Se a Conta não foi encontrada</response>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("{idParam}")]
+        public IActionResult Delete(int idParam)
+        {            
+            try
+            {
+                //gerar lista
+                var contas = GerarLista();
+
+                //selecionar a conta desejada a remover
+                var contaASerRemovida = contas.FirstOrDefault(item => item.Id == idParam);
+
+                //Retorna se foi removido ou não
+                var seRemovido = contas.Remove(contaASerRemovida);
+
+                if (seRemovido)
+                    return Ok(contaASerRemovida);
+                else
+                    throw new Exception();
+            }
+            catch (Exception e)
+            {
+                return NotFound();
+            }
+        }
+
+        #region Métodos Privados
         private List<ContaPF> GerarLista()
         {
             var rng = new Random();
@@ -62,68 +157,6 @@ namespace Financial.WebAPI.Controllers
             return lista;
         }
 
-
-        private readonly ILogger<ContaPFController> _logger;
-
-        public ContaPFController(ILogger<ContaPFController> logger)
-        {
-            _logger = logger;
-        }
-
-
-        /// <summary>
-        /// Lista de contas pessoa física
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public IEnumerable<ContaPF> GetAll()
-        {
-            //var rng = new Random();
-            //return Enumerable.Range(1, 1).Select(index => new ContaPF //Arrow Function
-            //{
-            //    Agencia = rng.Next(1111, 9999),
-            //    Conta = rng.Next(111111, 999999),
-            //    TipoConta = TipoConta[rng.Next(TipoConta.Length)],
-            //    NomeCompleto = Nome[rng.Next(Nome.Length)],
-            //})
-            //.ToList();
-
-            return GerarLista();
-        }
-
-
-        // GET api/<ContaPFController>/5
-        [HttpGet("{id}")]
-        public ContaPF GetById(int id)
-        {
-            return GerarLista()
-                .FirstOrDefault(conta => conta.Id == id);
-        }
-
-        // POST api/<ContaPFController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<ContaPFController>/5
-        [HttpPut("{id}")]
-        public ContaPF Put(int id, [FromBody] ContaPF contaPF)
-        {
-            var conta = GetById(id);
-
-            conta.Agencia = contaPF.Agencia;
-            conta.Conta = contaPF.Conta;
-            conta.TipoConta = contaPF.TipoConta;
-            conta.NomeCompleto = contaPF.NomeCompleto;
-
-            return conta;
-        }
-
-        // DELETE api/<ContaPFController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        #endregion Métodos Privados
     }
 }
