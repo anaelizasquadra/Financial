395a886f4250398da699caa0a793234e009a9d6e
Ana Eliza Bastos Martins de Freitas <ana.eliza@squadra.com.br>
2020-10-23 15:01:24 -0300

    Criação de bibliotecas de classe para a arquitetura

---
+++ WebAPI/Models/Response/UsuarioResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Financial.WebAPI.Models.Response
+{
+    public class UsuairoReponseModel
+    {
+    }
+}
