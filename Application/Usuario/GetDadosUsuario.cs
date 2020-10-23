7941d8eb444bd1832dcbf2b67a859fa8f680caa6
Ana Eliza Bastos Martins de Freitas <ana.eliza@squadra.com.br>
2020-10-23 15:56:28 -0300

    Aplicação do mediator

--- Application/Usuario/GetDadosUsuario.cs
+++ Application/Usuario/GetDadosUsuario.cs
@@ -6,5 +6,11 @@ namespace Financial.Application.Usuario
 {
     public class GetDadosUsuario
     {
+        private int id;
+
+        public GetDadosUsuario(int id)
+        {
+            this.id = id;
+        }
     }
 }
--- WebAPI/Controllers/UsuarioController.cs
+++ WebAPI/Controllers/UsuarioController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Financial.Application.Usuario;
+using Financial.WebAPI.Models.Response;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +14,24 @@ namespace Financial.WebAPI.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private readonly IMediator mediator;
+
+        public UsuarioController(IMediator _mediator)
+        {
+            mediator = _mediator;
+        }
+
+        /// <summary>
+        /// Método utilizado para retornar os dados do usuário
+        /// </summary>
+        /// <remarks>Dados retornados, filtrado pelo id do usuário desejado</remarks>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<UsuarioResponseModel> Get(int id)
+        {
+            var dadosUsuario = mediator.Send(new GetDadosUsuario(id));
+            return Ok();
+        }
     }
 }
