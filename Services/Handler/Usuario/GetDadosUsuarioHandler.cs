e2d1fd0bb84e2282ad199b1b2c9147a3c357b341
Ana Eliza Bastos Martins de Freitas <ana.eliza@squadra.com.br>
2020-10-27 10:33:50 -0300

    Inclusão da pasta VO

---
+++ Services/Handler/Usuario/GetDadosUsuarioHandler.cs
@@ -0,0 +1,23 @@
+﻿using Financial.Application.Usuario;
+using Financial.Application.ValueObject;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Financial.Services.Handler.Usuario
+{
+    public class GetDadosUsuarioHandler : IRequestHandler<GetDadosUsuario, UsuarioVO>
+    {
+        public GetDadosUsuarioHandler()
+        {
+        }
+
+        public Task<UsuarioVO> Handle(GetDadosUsuario request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
